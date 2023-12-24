19186a2724f38ce676a0136f5d1144bbe19480ec
weiraneve <907221539@qq.com>
2023-12-24 16:44:25 +0800

    [frontend]feat: add menu

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -1,8 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod menu;
+
 fn main() {
   tauri::Builder::default()
+    .menu(menu::init())
+    .on_menu_event(menu::menu_handler)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
